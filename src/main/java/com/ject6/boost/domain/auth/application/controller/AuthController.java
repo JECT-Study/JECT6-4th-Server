30a073f6f5d5540b0947e7e30c8117677a63d4e8
Cho-Hyun-Seung <toki0327@naver.com>
2026-05-30 11:50:11 +0900

    feat: auth 초기 설정

---
+++ src/main/java/com/ject6/boost/domain/auth/application/controller/AuthController.java
@@ -0,0 +1,19 @@
+package com.ject6.boost.domain.auth.application.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController( "/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+
+    /* TODO 로그인 기능 필요 */
+
+
+
+    /* TODO 로그 아웃 기능 필요 */
+
+
+    /* TODO 리프레시 토큰 갱신 기능 필요 */
+}
---
+++ src/main/java/com/ject6/boost/domain/auth/presentaion/service/AuthService.java
@@ -0,0 +1,9 @@
+package com.ject6.boost.domain.auth.presentaion.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+}
